fcbd64cbe1ec8a70054a877b4b0f837271bc331e
abhishekchugh17 <31280627+abhishekchugh17@users.noreply.github.com>
2017-10-30 20:05:52 +0530

    necessary changes

--- gen/com/android/forb/app/BuildConfig.java
+++ gen/com/android/forb/app/BuildConfig.java
@@ -1,6 +1,6 @@
 /** Automatically generated file. DO NOT MODIFY */
 package com.android.forb.app;
-
+// necessary changes
 public final class BuildConfig {
     public final static boolean DEBUG = true;
 }
